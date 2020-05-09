95597a6c97134d0fd4ef35aa01f3dd82a8da7553
”thorimitsu” <horimitsu.recruit@gmail.com>
2020-05-09 06:46:31 +0000

    レッスン実施

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -29,7 +29,7 @@ class NewsController extends Controller
       }
       
       unset($form['_token']);
-      unset($form['_image']);
+      unset($form['image']);
       
       $news->fill($form);
       $news->save();
@@ -47,5 +47,44 @@ class NewsController extends Controller
       }
       
       return view('admin.news.index',['posts' => $posts,'cond_title' => $cond_title]);
+    }
+    
+    public function edit(Request $request){
+      
+      $news = News::find($request->id);
+      if(empty($news)){
+        abort(404);
+      }
+      return view('admin.news.edit',['news_form'=>$news]);
+      
+    }
+    
+    public function update(Request $request){
+      
+      $this->validate($request, News::$rules);
+      $news = News::find($request->id);
+      $news_form = $request->all();
+      if(isset($news_form['image'])){
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+        unset($news_form['image']);
+      } elseif(isset($request->remove)) {
+        $news->image_path = null;
+        unset($news_form['remove']);
+      }      
+      
+      unset($news_form['_token']);
+      
+      $news->fill($news_form)->save();
+      
+      return redirect('admin/news/');
+    }
+    
+    public function delete(Request $request){
+      
+      $news = News::find($request->id);
+      $news->delete();
+      
+      return redirect('admin/news/');
     }
 }
--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -42,6 +42,10 @@
                                     <th>{{ $news->id }}</th>
                                     <td>{{ \Str::limit($news->title, 100) }}</td>
                                     <td>{{ \Str::limit($news->body, 250) }}</td>
+                                    <td>
+                                        <div><a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a></div>
+                                        <div><a href="{{ action('Admin\NewsController@delete', ['id' => $news->id]) }}">削除</a></div>                                    
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
