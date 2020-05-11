7e908044c74ed033c020a375f0be7f019dc0357b
”thorimitsu” <horimitsu.recruit@gmail.com>
2020-05-11 15:31:12 +0000

    プロフィールのindexファイル追加

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -3,50 +3,43 @@
 
 
 {{-- profile.blade.phpの@yield('title')に'MYプロフィール'を埋め込む --}}
-@section('title', 'MYプロフィール新規作成')
+@section('title', 'MYプロフィール')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>MYプロフィール新規作成</h2>
-                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
-
-                    @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
+                <h2>MYプロフィール</h2>
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            <input type="text" class="form-control" name="name" value="{{ $posts->name }}" readonly="readonly">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
+                            <input type="text" class="form-control" name="gender" value="{{ $posts->gender }}" readonly="readonly">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $posts->hobby }}" readonly="readonly">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label class="col-md-2">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20" readonly="readonly">{{ $posts->introduction }}</textarea>
                         </div>
                     </div>
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
-                </form>
+                    <div class="form-group row">
+                        <div class="col-auto mx-auto">
+                            <a href="{{ action('Admin\ProfileController@edit', ['id' => $posts->id]) }}"><button class="btn btn-primary" type="button">編集</button></a>
+                        </div>
+                    </div>
             </div>
         </div>
     </div>
